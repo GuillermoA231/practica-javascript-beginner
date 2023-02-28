5eae28835e42171c89776dde39d9fc664c5f6f63
GuillermoA231 <andradajoaquin302@gmail.com>
2023-02-28 17:27:00 -0300

    solucionado el undecimo y ultimo ejercicio, que lindo che

---
+++ ejercicios/ejercicio11.js
@@ -0,0 +1,15 @@
+let num = prompt("Introduce un numero:");
+let divisor = '';
+if (num % 2 === 0) {
+    divisor = '2';
+} else if(num % 3 === 0){
+    divisor = '3';
+} else if(num % 5 === 0){
+    divisor = '5';
+} else if(num % 7 === 0){
+    divisor = '7';
+}
+
+if (divisor != '') {
+    alert(`El número ${num} es divisible por ${divisor}`);
+}
