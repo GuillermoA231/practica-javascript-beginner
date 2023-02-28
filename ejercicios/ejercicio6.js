762ec19518b6277e051b898719f7ebd9d2976a6a
GuillermoA231 <andradajoaquin302@gmail.com>
2023-02-27 21:44:33 -0300

    solucionado sexto  ejercicio

---
+++ ejercicios/ejercicio6.js
@@ -0,0 +1,10 @@
+let numero1 = parseInt(prompt("Contame un número:"));
+let numero2 = parseInt(prompt("tirame otro número bro:"));
+
+if(numero1 > numero2){
+  document.write("El número más fachero es: " + numero1);
+} else if(numero2 > numero1){
+  document.write("El número más antofagasta es: " + numero2);
+} else {
+  document.write("Los dos números son iguales guachin, fijate bien que pones");
+}
