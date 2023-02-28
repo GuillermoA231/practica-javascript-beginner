33bdd6ad71ae0fdf8f67043b308cd68213f2eb3e
GuillermoA231 <andradajoaquin302@gmail.com>
2023-02-28 17:14:32 -0300

    solucionado decimo  ejercicio

---
+++ ejercicios/ejercicio10.js
@@ -0,0 +1,10 @@
+let num = prompt("ingresa un numero:");
+if (num % 2 === 0) {
+    alert("es divisible por 2.");
+} else if(num % 3 === 0){
+    alert("es divisible por 3.");
+} else if(num % 7 === 0){
+    alert("es divisible por 5.");
+} else if(num % 5 === 0){
+    alert("es divisible por 7.");
+}
