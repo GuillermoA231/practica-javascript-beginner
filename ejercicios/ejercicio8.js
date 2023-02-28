d59a28931ec1402711f3c8c059494dc79c365df8
GuillermoA231 <andradajoaquin302@gmail.com>
2023-02-27 21:47:28 -0300

    solucionado octavo  ejercicio

---
+++ ejercicios/ejercicio8.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Introduce un número:"));
+
+if (numero % 2 === 0) {
+  document.write("El número " + numero + " es divisible por 2");
+} else {
+  document.write("El número " + numero + " no es divisible por 2");
+}
