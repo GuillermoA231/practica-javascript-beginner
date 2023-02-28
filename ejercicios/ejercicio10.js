daf729f335dc422b93742265fcd4ea670f1a217d
GuillermoA231 <andradajoaquin302@gmail.com>
2023-02-28 17:18:02 -0300

    arregla orden de los else if, el 5 y el 7 estaban mal posicionados

--- ejercicios/ejercicio10.js
+++ ejercicios/ejercicio10.js
@@ -3,8 +3,8 @@ if (num % 2 === 0) {
     alert("es divisible por 2.");
 } else if(num % 3 === 0){
     alert("es divisible por 3.");
-} else if(num % 7 === 0){
-    alert("es divisible por 5.");
 } else if(num % 5 === 0){
+    alert("es divisible por 5.");
+} else if(num % 7 === 0){
     alert("es divisible por 7.");
 }
