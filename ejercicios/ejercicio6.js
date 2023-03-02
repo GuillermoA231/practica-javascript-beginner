cddb588d812703173edec36e6c59963853320f6e
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-02 06:53:32 -0300

    cambia console.log a alert

--- ejercicios/ejercicio6.js
+++ ejercicios/ejercicio6.js
@@ -2,9 +2,9 @@ let numero1 = parseInt(prompt("Contame un número:"));
 let numero2 = parseInt(prompt("tirame otro número bro:"));
 
 if(numero1 > numero2){
-  console.log("El número más fachero es: " + numero1);
+  alert("El número más fachero es: " + numero1);
 } else if(numero2 > numero1){
-  console.log("El número más grandote es: " + numero2);
+  alert("El número más grandote es: " + numero2);
 } else {
-  console.log("Los dos números son iguales guachin, fijate bien que pones");
+  alert("Los dos números son iguales guachin, fijate bien que pones");
 }
