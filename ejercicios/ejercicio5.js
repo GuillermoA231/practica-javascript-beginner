9d02e25c8de6e4dd309a4863f111e2a3251601bd
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-02 06:51:04 -0300

    arregla el ejercicio5, mal llamamiento de variables y cambia document.write por console.log

--- ejercicios/ejercicio5.js
+++ ejercicios/ejercicio5.js
@@ -1,3 +1,3 @@
 let num1 = parseInt(prompt("Introduce el primer número:"));
 let num2 = parseInt(prompt("Introduce el segundo número:"));
-document.write("El resultado es: " + (numero1 + numero2));
+console.log("El resultado es: " + (num1 + num2));
