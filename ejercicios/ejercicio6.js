4c4db7ece9fd23c7454606838dff16fa2d71895d
GuillermoA231 <andradajoaquin302@gmail.com>
2023-03-02 06:52:12 -0300

    arregla el ejercicio6 para agregar console.logs y cambia un poco el texto

--- ejercicios/ejercicio6.js
+++ ejercicios/ejercicio6.js
@@ -2,9 +2,9 @@ let numero1 = parseInt(prompt("Contame un número:"));
 let numero2 = parseInt(prompt("tirame otro número bro:"));
 
 if(numero1 > numero2){
-  document.write("El número más fachero es: " + numero1);
+  console.log("El número más fachero es: " + numero1);
 } else if(numero2 > numero1){
-  document.write("El número más antofagasta es: " + numero2);
+  console.log("El número más grandote es: " + numero2);
 } else {
-  document.write("Los dos números son iguales guachin, fijate bien que pones");
+  console.log("Los dos números son iguales guachin, fijate bien que pones");
 }
